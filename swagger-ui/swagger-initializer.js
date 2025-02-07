877e3072bfb900263d194141f3554769edcc4346
froggy1014 <clcl6084@gmail.com>
2025-02-07 11:34:58 +0900

    🔧 chore: add Oauth header parameter temporary

--- swagger-ui/swagger-initializer.js
+++ swagger-ui/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://petstore.swagger.io/v2/swagger.json", 
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -15,12 +15,5 @@ window.onload = function() {
     ],
     layout: "StandaloneLayout",
     oauth2RedirectUrl: "https://whatsapp-typescript-axios.vercel.app/oauth2-redirect.html"
-  });
-
-  ui.initOAuth({
-    clientId: "clientId",
-    clientSecret: "clientSecret",
-    scopes: ["whatsapp_business_management", "whatsapp_business_messaging", "business_management"],
-    useBasicAuthenticationWithAccessCodeGrant: true
   });
 };
