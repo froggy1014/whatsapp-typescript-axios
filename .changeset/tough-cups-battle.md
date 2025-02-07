054844cc99a306db0fe625667c8966d991870e16
froggy1014 <clcl6084@gmail.com>
2025-02-07 17:27:40 +0900

    🔧 chore: upload session refactor

---
+++ .changeset/tough-cups-battle.md
@@ -0,0 +1,5 @@
+---
+"whatsapp-typescript-axios": patch
+---
+
+update upload-session-id
--- swagger-ui/swagger-initializer.js
+++ swagger-ui/swagger-initializer.js
@@ -1,30 +1,7 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
-  // Custom plugin to modify token request
-  const customPlugin = {
-    statePlugins: {
-      auth: {
-        wrapActions: {
-          authorize: (oriAction, system) => (auth) => {
-            const authConfigs = system.getConfigs().authConfigs;
-            const oauthConfig = authConfigs.find(config => config.type === 'oauth2');
-            if (oauthConfig) {
-              const tokenRequest = oauthConfig.tokenRequest;
-              // Modify the token request to use query parameters
-              const params = new URLSearchParams();
-              for (const [key, value] of Object.entries(tokenRequest.body)) {
-                params.append(key, value);
-              }
-              tokenRequest.url += `?${params.toString()}`;
-              tokenRequest.body = null; // Remove form data
-            }
-            return oriAction(auth);
-          }
-        }
-      }
-    }
-  };
+
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
@@ -41,12 +18,6 @@ window.onload = function() {
     ],
     layout: "StandaloneLayout",
     oauth2RedirectUrl: "https://whatsapp-typescript-axios.vercel.app/swagger-ui/oauth2-redirect.html",
-    requestInterceptor: (req) => {
-      const token = localStorage.getItem('access_token'); // or wherever you store the token
-      if (token) {
-        req.headers['Authorization'] = `OAuth ${token}`;
-      }
-      return req;
-    }
+
   });
 };
